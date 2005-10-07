3bf6493167838b093a6493d8d05b9faf48517084
peteru <peteru@04cd18b3-d4fa-0310-ad16-b4e37cca70a5>
2005-10-07 05:38:17 +0000

    Add missing header file.

--- trunk/taps/VirtualRemote/VirtualRemote.c
+++ trunk/taps/VirtualRemote/VirtualRemote.c
@@ -19,6 +19,7 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+#include <string.h>
 #include <tap.h>
 
 TAP_ID( 0x810a0012 );
