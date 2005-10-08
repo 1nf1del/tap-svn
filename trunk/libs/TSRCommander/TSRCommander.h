5b434125fa2a1db1695a5a9a7d9529d242ed50e2
simonc <simonc@04cd18b3-d4fa-0310-ad16-b4e37cca70a5>
2005-10-08 01:40:49 +0000

    Improved TSRCommander.h and usage example

---
+++ trunk/libs/TSRCommander/TSRCommander.h
@@ -0,0 +1,136 @@
+// Improved version of TSRCommander.inc
+// Include TSRCommander.h at the top of your TAP's main source file after <tap.h>
+// implement TSRCommanderConfigDialog to create your configuration menu or leave the function body
+// empty if there is no configuration
+// Implement TSRCommanderExitTAP to clean up when your TAP exits. Return TRUE to allow the TAP to
+// close, or FALSE if
+// In your TAP_Main, return TAP_TSR | TAP_HASCONFIG to make TSRCommander display a config menu item
+
+#ifndef __TSRCOMMANDER_H
+#define __TSRCOMMANDER_H
+
+
+// Prototypes for TSR Commander 
+// These *must* be implemented by a TSR Commander compliant TAP 
+void TSRCommanderConfigDialog();
+bool TSRCommanderExitTAP();
+
+// Flags returned to TSRCommander from TAP_Main
+// Intentionally starting at 2 for backwards compatibilty with old TAP_Mains
+#define TAP_HASCONFIGMENU 2
+
+extern const char __tap_program_name__[];
+
+typedef struct
+{
+	char	landingzone[256];
+	char	signature[20];
+	char	*TAPName;
+	dword	ExitTAP;
+	dword	HasConfigDialog;
+	dword	ShowConfigDialog;
+	dword	FutureExtensions[64];
+} TYPE_TSRCommander;
+
+static TYPE_TSRCommander TSRCommander;
+
+
+void TSRCommanderErase()
+{
+	int i;
+
+	for ( i=0 ; i < 256 ; i++ )					// erase landingzone
+	{
+		TSRCommander.landingzone[i] = ' ';
+	}
+	for ( i=0 ; i < 20 ; i++ )					// erase signature
+	{
+		TSRCommander.signature[i] = ' ';
+	}
+}
+
+
+// Prototype for TAP
+dword TSR_EventHandler(word event, dword param1, dword param2 );
+
+// Main event handler
+dword TAP_EventHandler( word event, dword param1, dword param2 )
+{
+	if ( event == EVT_IDLE )
+	{
+		if (TSRCommander.ShowConfigDialog)
+		{
+			TSRCommander.ShowConfigDialog = FALSE;
+			TSRCommanderConfigDialog();
+		}
+		if (TSRCommander.ExitTAP == TRUE)
+		{
+			TSRCommander.ExitTAP = FALSE;
+			if (TSRCommanderExitTAP() == TRUE)
+			{
+				TSRCommanderErase();				// destroy TSRCommander-signature
+				TAP_Exit();					// usual exit-call for TSR-TAP
+			}
+		}
+	}
+	
+	// Call TAP's event handler
+	return TSR_EventHandler( event, param1, param2 );
+}
+
+// Prototype for TAP
+int TSR_Main();
+
+// Main entry point
+int TAP_Main()
+{
+	int result;
+	int i;
+
+	for ( i=0 ; i < 256 ; i++ )					// preparing landingzone for faster RAM-scann
+	{
+		TSRCommander.landingzone[i] = 'J';
+	}
+	TSRCommander.signature[0] = 'J';				// set signature-value
+	TSRCommander.signature[1] = 'u';
+	TSRCommander.signature[2] = 'd';
+	TSRCommander.signature[3] = 'y';
+	TSRCommander.signature[4] = 's';
+	TSRCommander.signature[5] = '_';
+	TSRCommander.signature[6] = 'S';
+	TSRCommander.signature[7] = 'i';
+	TSRCommander.signature[8] = 'g';
+	TSRCommander.signature[9] = 'n';
+	TSRCommander.signature[10] = 'a';
+	TSRCommander.signature[11] = 't';
+	TSRCommander.signature[12] = 'u';
+	TSRCommander.signature[13] = 'r';
+	TSRCommander.signature[14] = 'e';
+	TSRCommander.signature[15] = '_';
+	TSRCommander.signature[16] = 'X';
+	TSRCommander.signature[17] = '7';
+	TSRCommander.signature[18] = '0';
+	TSRCommander.signature[19] = 'E';
+	TSRCommander.TAPName = (char *) __tap_program_name__;		// set tap-name pointer
+	TSRCommander.ExitTAP = FALSE;					// dont stop tap on startup
+	TSRCommander.HasConfigDialog = FALSE;			// set to TRUE if you have a config-dialog
+	TSRCommander.ShowConfigDialog = FALSE;				// dont show config-dialog at startup
+	for ( i=0 ; i < 64 ; i++ )					// deleting extension
+	{
+		TSRCommander.FutureExtensions[i] = 0;
+	}
+
+	// Call TAP's Main function.
+	// Return value dictates whether TSR Commander shows a config menu item
+	result = TSR_Main();
+	TSRCommander.HasConfigDialog = (result & TAP_HASCONFIGMENU) != 0;
+	return result > 0;
+}
+
+
+// Redefine normal TAP entry points becone TSR_*
+#define TAP_EventHandler TSR_EventHandler
+#define TAP_Main TSR_Main
+
+
+#endif
---
+++ trunk/libs/TSRCommander/example/Example.c
@@ -0,0 +1,66 @@
+/*
+Copyright (C) 2005 Simon Capewell
+
+This file is part of the TAPs for Topfield PVRs project.
+	http://tap.berlios.de/
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <tap.h>
+#include "TSRCommander.h"
+
+TAP_ID( 0x81030504 );
+TAP_PROGRAM_NAME("Example");
+TAP_AUTHOR_NAME("Simon Capewell");
+TAP_DESCRIPTION("Sample TSR Commander compatible TAP");
+TAP_ETCINFO(__DATE__);
+
+
+//----------------------------------------------------------------------------
+// This function cleans up and closes the TAP
+bool TSRCommanderExitTAP()
+{
+	return TRUE;
+}
+
+
+//-----------------------------------------------------------------------------
+// This function displays a config menu if the TAP implements one
+void TSRCommanderConfigDialog()
+{
+}
+
+
+//-----------------------------------------------------------------------------
+dword TAP_EventHandler( word event, dword param1, dword param2 )
+{
+	if ( event == EVT_IDLE )
+	{
+	}
+	else if ( event == EVT_KEY )
+	{
+	}
+
+	return param1;
+}
+
+//-----------------------------------------------------------------------------
+int TAP_Main()
+{
+	// Return 1 if the TAP has no config menu, or TAP_HASCONFIGMENU if it does
+	// and you want TSR Commander to show a config menu item for it.
+	return 1; 
+}
