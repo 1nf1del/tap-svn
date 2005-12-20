e481f21edcced8a87fc637c3f1feaecb6bdca871
simonc <simonc@04cd18b3-d4fa-0310-ad16-b4e37cca70a5>
2005-12-20 21:24:11 +0000

    Added fixes for TAP_Hdd_Flen bugs

--- trunk/libs/libutils/tapapifix.c
+++ trunk/libs/libutils/tapapifix.c
@@ -1,11 +1,15 @@
+#include <stdio.h>
 #include <tap.h>
 #include "tapapifix.h"
 
 
-// Workaround functions for buffer overrun problems
 #undef TAP_Hdd_GetPlayInfo
 #undef TAP_Hdd_GetRecInfo
+#undef TAP_Hdd_Flen
+#undef TAP_Hdd_Fseek
 
+
+// Workaround for TAP_Hdd_GetPlayInfo and TAP_Hdd_GetRecInfo buffer overrun bugs
 bool Safe_TAP_Hdd_GetPlayInfo( TYPE_PlayInfo* playInfo )
 {
 	static TYPE_PlayInfo safeInfo;
@@ -45,4 +49,23 @@ bool Safe_TAP_Hdd_GetRecInfo( byte recSlot, TYPE_RecInfo* recInfo )
 	memcpy( recInfo, &safeInfo, sizeof(TYPE_RecInfo) );
 	
 	return TRUE;
+}
+
+
+// Workaround for TAP_Hdd_Flen multiple of 512 bytes bug
+// If a file is a multiple of 512 bytes then TAP_Hdd_Flen returns 512 bytes too little
+dword Fixed_TAP_Hdd_Flen( TYPE_File *file )
+{
+	dword length = TAP_Hdd_Flen( file );
+	return length == 0 || (length % 512) ? length : (length + 512);
+}
+
+
+// and TAP_Hdd_Fseek SEEK_END works incorrectly
+dword Fixed_TAP_Hdd_Fseek( TYPE_File *file, long pos, long where )
+{
+	if ( where != SEEK_END || (file && (file->size == 0 || (file->size % 512))) )
+		return TAP_Hdd_Fseek( file, pos, where );
+
+	return TAP_Hdd_Fseek( file, pos+512, SEEK_END );
 }
