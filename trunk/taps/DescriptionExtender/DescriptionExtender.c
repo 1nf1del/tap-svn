31575dfa52aea820f2deb221a66225d68aea8c70
simonc <simonc@04cd18b3-d4fa-0310-ad16-b4e37cca70a5>
2006-02-21 01:51:24 +0000

    Description Extender performance enhancements

--- trunk/taps/DescriptionExtender/DescriptionExtender.c
+++ trunk/taps/DescriptionExtender/DescriptionExtender.c
@@ -1,14 +1,36 @@
+/*
+	Copyright (C) 2005 Simon Capewell
+
+	This file is part of the TAPs for Topfield PVRs project.
+		http://tap.berlios.de/
+
+	This is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	The software is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this software; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 #include <tap.h>
 #include <string.h>
+#include <firmware.h>
 #include <OPCodes.h>
+#include <exTap.h>
 #include <messagewin.h>
 
 
-TAP_ID( 0x81243234 );
-TAP_PROGRAM_NAME("DescriptionExtender 0.3 beta");
+TAP_ID( 0x81243235 );
+TAP_PROGRAM_NAME("DescriptionExtender 1.0 Beta 5");
 TAP_AUTHOR_NAME("Simon Capewell");
-TAP_DESCRIPTION("Makes longer programme descriptions");
-TAP_ETCINFO("descriptions available to TAPs");
+TAP_DESCRIPTION("Provides long TAP EPG descriptions");
 
 
 #include <TSRCommander.h>
@@ -56,19 +78,10 @@ void* FW_MemAlloc(dword size)
 int FW_MemFree(const void *addr)
 {
 }
-TYPE_TapEvent* FW_GetEvent( int svcType, int svcNum, int *eventNum )
-{
-}
-int FW_Channel_GetFirstInfo( int svcType, TYPE_TapChInfo *chInfo )
-{
-}
-int FW_Channel_GetNextInfo( TYPE_TapChInfo *chInfo )
-{
-}
 void FW_Print(const void *fmt, ...)
 {
 }
-void* AddressOfHack()
+void* AddressOfEventTable()
 {
 	__asm__ __volatile__ (
 		"lui	$02,0x0\n"
@@ -78,166 +91,129 @@ void* AddressOfHack()
 }
 
 
-// Get the Topfield channel number, given a service id
-bool FindChannel( int svcId, int* svcType, int* svcNum )
-{
-	if ( !svcType || !svcNum )
-		return FALSE;
-
-	for ( *svcType = 0; *svcType <= 1; ++*svcType )
-	{
-		TYPE_TapChInfo chInfo;
-		*svcNum = 0;
-		if ( FW_Channel_GetFirstInfo( *svcType, &chInfo ) )
-		do
-		{
-			if ( chInfo.svcId == svcId )
-			{
-				return TRUE;
-			}
-			++*svcNum;
-		}
-		while ( FW_Channel_GetNextInfo( &chInfo ) );
-	}
-
-	return FALSE;
-}
-
-
-#define ADDR(addr, offset) ((word*)((word*)addr)+1+((offset)>>1))
-
-
+// Read description from the firmware's EPG event table
 byte* GetEventDescription( TYPE_TapEvent* event )
 {
 	byte* result = NULL;
 
 	if ( event && event->svcId )
 	{
-		int svcType;
-		int svcNum;
-		if ( FindChannel( event->svcId, &svcType, &svcNum ) )
+		type_eventtable* et = (type_eventtable*)AddressOfEventTable();
+		if ( et )
 		{
-			int eventNum;
-			TYPE_TapEvent* allEvents;
-
-			// Sanity check before we temporarily modify TAP_GetEvent
-			if ( *ADDR(AddressOfHack(), 0) != 0x007f ||
-				 *ADDR(AddressOfHack(), 0x4c) != 0x007f ||
-				 *ADDR(AddressOfHack(), 0x58) != 0x008f ||
-				 *ADDR(AddressOfHack(), 0x60) != 0x008f )
+			// Try each entry in the event table. Not very efficient, but the firmware does exactly the same
+			int i;
+			for (i=0; i<5000; i++)
 			{
-				FW_Print( "TAP_GetEvent failed\n" );
-				return result;
-			}
+				type_eventtable* e = &et[i];
 
-			*ADDR(AddressOfHack(), 0) = 0;				// copy 0 characters of name
-			*ADDR(AddressOfHack(), 0x4c) = 0x00ff;		// copy 255 characters of description
-			*ADDR(AddressOfHack(), 0x58) = 0x000f;		// start of description=start of name
-			*ADDR(AddressOfHack(), 0x60) = 0x000f;
-
-			allEvents = FW_GetEvent( svcType, svcNum, &eventNum );
-			if ( allEvents )
-			{
-				int i;
-				for( i=0; i < eventNum; ++i )
+				// Match satIdx, orgNetId, tsId, svcId, evtId
+				if (event->evtId == e->event_id)
 				{
-					if ( allEvents[i].evtId == event->evtId )
+					if (event->svcId == e->service_id && event->tsId == e->transport_stream_id &&
+						event->orgNetId == e->original_network_id && event->satIdx == e->sat_index)
 					{
-						int length =  strlen(allEvents[i].eventName);
-						if ( length > 127 )
-						{
-							// split description between description and extended info
-							//result = FW_MemAlloc( length-126 );
-							//strcpy( result, allEvents[i].eventName+127 );
+						// Get the address of the description string
+						byte *description = (char *)((dword)e->event_name + e->event_name_length);
+						// Work out the length of the string we're going to return
+						int extended_length = e->extended_length;
+						int description_length = strlen(description);
+						if ( description_length > 127 )
+							extended_length += description_length+1;
 
-							// entire description returned in extended info
-							result = FW_MemAlloc( length+1 );
-							strcpy( result, allEvents[i].eventName );
+						// If there's going to be something worth returning
+						if ( extended_length > 0 )
+						{
+							// allocate memory plus extra for zero terminators
+							result = FW_MemAlloc(extended_length+2);
+							if ( result )
+							{
+								byte* p = result;
+								if ( description_length > 127 )
+								{
+									// There's a long description to return
+									memcpy(p, description, description_length);
+									p[description_length] = '\0';
+									p+=description_length;
+								}
+								// Append the existing extended infomation
+								memcpy(p, e->extended_event_name, e->extended_length);
+								p[e->extended_length] = '\0';
+								// And terminate with an additional terminator
+								p[e->extended_length+1] = '\0';
+							}
 						}
 						break;
 					}
 				}
-				FW_MemFree( allEvents );
 			}
-			else
-			{
-				FW_Print("FW_GetEvent returned no events\n");
-			}
-
-			// restore original firmware values
-			*ADDR(AddressOfHack(), 0) = 0x007f;
-			*ADDR(AddressOfHack(), 0x4c) = 0x007f;
-			*ADDR(AddressOfHack(), 0x58) = 0x008f;
-			*ADDR(AddressOfHack(), 0x60) = 0x008f;
 		}
 	}
 
 	return result;
 }
 
+typedef struct {
+	Model	model;
+    int		firmwareVersion;
+    dword	eventTable;
+} FirmwareDetail;
 
-
-dword eventFillSignature[] =
+FirmwareDetail firmware[] = 
 {
-	0x2418007f,
-	0x0302082b,
-	0x10200003,
-	0x24190118,
-	0x03001025,
-	0x24190118,
-	0x00000000,
-	0x02ff0019,
-	0x00009812,
-	0x02ffff21,
-	0xffc4000f,
-	0x02802825,
-	0x0c00ffff,
-	0x00403025,
-	0x02802025,
-	0x0c05ffff,
-	0xffc02825,
-	0x0c000dff,
-	0x02802025,
-	0x2419007f,
-	0x0322082b,
-	0x10200003,
-	0xffc4008f,
-	0x03201025,
-	0xffc4008f
+	TF5800t,		0x1225,	0x8032a7c8,
+	TF5800t,		0x1209,	0x80326c4c,
+	TF5800t,		0x1205,	0x8032e818,
+	TF5800t,		0x1204,	0x8032e698,
+	TF5000_5500,	0x1205,	0x802d6bb4,
+	TF5100,			0x1205,	0x802ad730, // Sept 05
+//	TF5100_MP,		0x1212,	0x802c7ab4, // Oct 04
+	TF5200c,		0x1205,	0x802ac8e8
 };
-
 
 int TAP_Main(void)
 {
-	dword addr;
+	dword eventTable;
+	bool supported = FALSE;
+	int index;
 
-	TAP_Print("Starting DescriptionExtender\n");
-
-	// Verify that the hack will work when it gets called
-	addr = FindFirmwareFunction( eventFillSignature, sizeof(eventFillSignature), TAP_GetEvent, ((dword*)TAP_GetEvent) + 0x100 );
-	if ( addr ==0 ||
-		 *ADDR(addr, 0) != 0x007f ||
-		 *ADDR(addr, 0x4c) != 0x007f ||
-		 *ADDR(addr, 0x58) != 0x008f ||
-		 *ADDR(addr, 0x60) != 0x008f )
+	// Look up the current firmware
+	for (index=0; index<sizeof(firmware)/sizeof(FirmwareDetail); ++index)
 	{
-		ShowMessage( "Sorry, this TAP is not compatible with your firmware", 200 );
+		if ( GetModel() == firmware[index].model && _appl_version == firmware[index].firmwareVersion )
+		{
+			supported = TRUE;
+			break;
+		}
 	}
 
+	if ( !supported )
+	{
+		// Try and get some helpful information of users with unsupported firmware
+		char buffer[300];
+		sprintf( buffer, "Sorry, this TAP is not compatible with your firmware\n"
+			"For an update, please contact the author quoting\n"
+			"%d, %04X and %X for an update", *sysID, _appl_version, TAP_EPG_GetExtInfo-0x80000000 );
+		ShowMessage( buffer, 750 );
+		return 0;
+	}
+
+	// We need to know the address of the firmware event table without needing to use $gp
+	((word*)AddressOfEventTable)[1] = ((dword)firmware[index].eventTable >> 16) & 0xffff;
+	((word*)AddressOfEventTable)[3] = (dword)firmware[index].eventTable & 0xffff;
+
 	// Replace TAP_EPG_GetExtInfo with a jump to our GetEventDescription function
-	HackFirmware( TAP_EPG_GetExtInfo, J(GetEventDescription) );
+	HackFirmware( (dword*)TAP_EPG_GetExtInfo, J(GetEventDescription) );
+	HackFirmware( ((dword*)TAP_EPG_GetExtInfo)+1, NOP_CMD ); // MUST insert a nop after the jump
 
 	// Set up TAP API wrappers that don't require the use of $gp to be called
-	*((dword*)FW_MemAlloc) = J(TAP_MemAlloc);
-	*((dword*)FW_MemFree) = J(TAP_MemFree);
-	*((dword*)FW_GetEvent) = J(TAP_GetEvent);
-	*((dword*)FW_Channel_GetFirstInfo) = J(TAP_Channel_GetFirstInfo);
-	*((dword*)FW_Channel_GetNextInfo) = J(TAP_Channel_GetNextInfo);
+	((dword*)FW_MemAlloc)[0] = J(TAP_MemAlloc);
+	((dword*)FW_MemAlloc)[1] = NOP_CMD;
+	((dword*)FW_MemFree)[0] = J(TAP_MemFree);
+	((dword*)FW_MemFree)[1] = NOP_CMD;
+	// Useful for debugging
 	((dword*)FW_Print)[0] = J(TAP_Print);
 	((dword*)FW_Print)[1] = NOP_CMD;
-	// We also need to know the address of TAP_GetEvent without needing to use $gp
-	((word*)AddressOfHack)[1] = ((dword)addr >> 16) & 0xffff;
-	((word*)AddressOfHack)[3] = (dword)addr & 0xffff;
 
 	return 1;
 }
--- trunk/taps/DescriptionExtender/readme.txt
+++ trunk/taps/DescriptionExtender/readme.txt
@@ -1,15 +1,44 @@
-DescriptionExtender 0.3 Beta 
+Description Extender 1.0 Beta 5
 
-The usual beta caution applies: don't try this whilst you are recording something important. It may crash your Toppy. I've only tested on firmware version 12.09.
+This TAP currently only supports the following firmware
+TF5800:			12.04, 12.05, 12.09, 12.25
+TF5200c:		1.45
+TF5100:			1.45
+TF5000/5500:	12.05
 
-This TAP extends the EPG programme description text that is visible to TAPs by returning a longer programme description (up to 256 characters) in the EPG extended data which is normally blank in the UK. No special coding is required by other TAPs to support this TAP, other than the use of the standard TAP_EPG_GetExtInfo API call.
+Description Extender extends the EPG programme description text that is visible to TAPs by returning a longer programme description (up to 256 characters) in the EPG extended data which is normally blank in the UK. No special support for Description Extender is required by other TAPs to see this information.
 
-The TAP can be stopped using TSRCommander.
+The TAP can be stopped using TAP Commander or TSR Commander.
 
-The design of the TAP architecture prevents easy lengthening of the description field from its 128 limit without breaking all existing TAPs that request data from the EPG, which is probably the reason Topfield have not addressed the issue already.
 
 Brief overview of the implementation
-The TAP replaces the default TAP_EPG_GetExtInfo function in the firmware with a call to a custom function. This new function temporarily hacks the TAP_GetEvent function to fill the eventName and description with a 256 character description. It then searches for the relevant TYPE_TapEvent in the list of results and returns a string allocated on the heap as you would normally expect from TAP_EPG_GetExtInfo.
+The design of the TAP architecture prevents easy lengthening of the description field from its 128 limit without breaking all existing TAPs that request data from the EPG, which is probably the reason Topfield have not addressed the issue already.
+
+The TAP replaces the default TAP_EPG_GetExtInfo function in the firmware with a call to a custom function. This function looks directly at the EPG tables to retrieve the full description plus the original extended information and returns a string allocated on the heap as you would normally expect from TAP_EPG_GetExtInfo.
+
+Supporting Description Extender in your TAP
+If Description Extender support isn't added to your TAP, users will see the first 127 characters of long descriptions duplicated when you show description and extended information. The following source code will overcome this problem.
+
+// Get the current event.
+TYPE_TapEvent* event = TAP_GetCurrentEvent( chType, chNum );
+// Get the extended info.
+byte* extInfo = TAP_EPG_GetExtInfo( &event );
+
+// Test for the circumstances where the Description Extender is running:
+// We have extended information AND the normal description matches the 
+// first 127 extended info characters.
+if ( extInfo && strncmp(event.description, extInfo, 127) == 0 )
+{
+	// only process extended info
+	TAP_Print( extInfo );
+}
+else
+{
+	// process both description and extended info
+	TAP_Print( event.description );
+	if ( extInfo )
+		TAP_Print( extInfo );
+}
 
 Simon Capewell
 simon.capewell@gmail.com
