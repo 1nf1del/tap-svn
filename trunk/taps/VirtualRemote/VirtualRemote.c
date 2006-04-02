da619fab9b8f4697c21f6a763020f463086c4fb5
simonc <simonc@04cd18b3-d4fa-0310-ad16-b4e37cca70a5>
2006-04-02 17:00:44 +0000

    Virtual Remote major rewrite

--- trunk/taps/VirtualRemote/VirtualRemote.c
+++ trunk/taps/VirtualRemote/VirtualRemote.c
@@ -21,40 +21,27 @@
 
 #include <string.h>
 #include <tap.h>
+#include "extap.h"
 #include "Firmware.h"
+#include "tapapifix.h"
+#include "TAPExtensions.h"
 #include "OpCodes.h"
+#include "ScreenCapture.h"
 
 
-TAP_ID( 0x810a0012 );
-TAP_PROGRAM_NAME("Virtual Remote");
+TAP_ID( 0x810a0013 );
+TAP_PROGRAM_NAME("Virtual Remote 1.0");
 TAP_AUTHOR_NAME("Simon Capewell");
 TAP_DESCRIPTION("Control your Toppy from your PC via the serial cable");
 TAP_ETCINFO(__DATE__);
 
 #include "TSRCommander.h"
 
-#define RX_BUF_SIZE 4096
-
-static bool echo = TRUE;
-static char rxBuffer[RX_BUF_SIZE];
-static int writeIndex = 0;
-static int readIndex = 0;
-
-dword kbHitCall;
-
-typedef void (*CmdParser)(char* cmdLine);
-CmdParser cmdParser = NULL;
-
 
 //----------------------------------------------------------------------------
 // This function cleans up and closes the TAP
 bool TSRCommanderExitTAP()
 {
-	UndoFirmwareHacks();
-
-	// free the code wrapper buffer
-	free( cmdParser );
-
 	// print exit message
 	TAP_Print(" \nVirtual Remote TAP closed\n ");
 
@@ -66,292 +53,169 @@ void TSRCommanderConfigDialog()
 {
 }
 
-//-----------------------------------------------------------------------------
-// This function processes the input buffer to get a line terminated by a carrige return (CR).
-// On occurence of CR the line is passed to the extended command handler in the firmware.
+
+bool IsRecording(int slot)
+{
+	TYPE_RecInfo recInfo;
+	memset( &recInfo, 0, sizeof(recInfo) );
+	return TAP_Hdd_GetRecInfo(slot, &recInfo) &&
+		(recInfo.recType == RECTYPE_Normal || recInfo.recType == RECTYPE_Copy);
+}
+
+
+typedef struct _Command
+{
+	byte length;
+	byte crc;
+	word command;
+	dword param1;
+	dword param2;
+	byte id;
+} Command;
+
+// Process serial input
 void ProcessInput()
 {
-	static char line[255];
-	static int length = 0;
-	char ch;
+	int i;
+	Command cmd;
+	byte* buffer = (byte*)&cmd;
+	byte crc = 0;
+	for ( i=0; i<13; ++i )
+		buffer[i] = TAP_GetCh();
+	for ( i=3; i<13; ++i )
+		crc += buffer[i];
 
-	while ( readIndex != writeIndex )
+	// Send a reply
+	TAP_PutByte(236);
+	TAP_PutByte(10);
+	TAP_PutByte(0);
+	if ( cmd.crc == crc )
 	{
-		ch = rxBuffer[readIndex];
-
-		// check the line length
-		if ( length >= (sizeof(line) - 1) )
+		// Success reply
+		TAP_PutByte(1);
+		TAP_PutByte(236);
+		switch ( cmd.command )
 		{
-			TAP_Print( "\nCommand line too long!\n" );
-			length = 0;
-
-			// display new prompt
-			if ( echo )
-				TAP_Print( "\n=> " );
+		case 1:
+			if ( cmd.param2 == 0 )
+				TAP_GenerateEvent( EVT_KEY, cmd.param1, 0 );
+			else
+			{
+				if ( cmd.param2 & 0x100 )
+					cmd.param2 = 0x10000 | (cmd.param2 & 0xff);
+				exTAP_CallFirmware( (void*)0x80005bb4, 0xe300, cmd.param2, 0, 0 );
+			}
+			break;
+		case 2:
+		{
+			ScreenCaptureFlags flags = cmd.param1;
+			if ( flags == 0 )
+				flags = SCREENCAP_TV | SCREENCAP_OSD | SCREENCAP_ALPHA;
+			CaptureScreenToFile(flags);
+			break;
 		}
-
-		switch(ch)
+		case 3:
 		{
-		case '\r':
-			// CR found
-			if ( !length )
-			{
-				// display new prompt
-				if ( echo )
-					TAP_Print("\n=> ");
-
-				break;
-			}
-
-			// terminate string
-			line[length] = 0;
-
-			if ( echo )
-				TAP_Print("\n");
-
-			// check for exit command
-			if ( stricmp(line, "exit") == 0 )
-			{
-				TSRCommanderExitTAP();
-				TAP_Exit();
-				return;
-			}
-			else if ( stricmp(line, "echo on") == 0 )
-			{
-				echo = TRUE;
-			}
-			else if ( stricmp(line, "echo off") == 0 )
-			{
-				echo = FALSE;
-			}
-			else if ( stricmp(line, "timeshift off") == 0)
-			{
-				TAP_SetSystemVar( SYSVAR_Timeshift, 0 );
-			}
-			else if ( stricmp(line, "timeshift on") == 0)
-			{
-				TAP_SetSystemVar( SYSVAR_Timeshift, 1 );
-			}
-			else if ( strcmpi(line, "key newf1") == 0 )
-			{
-				TAP_GenerateEvent( EVT_KEY, RKEY_NewF1, 0 );
-			}
-			else if ( stricmp(line, "key <<") == 0 )
-			{
-				TAP_GenerateEvent( EVT_KEY, RKEY_Prev, 0 );
-			}
-			else if ( stricmp(line, "key >>") == 0 )
-			{
-				TAP_GenerateEvent( EVT_KEY, RKEY_Next, 0 );
-			}
-			else if ( stricmp(line, "key sat") == 0 )
-			{
-				TAP_GenerateEvent( EVT_KEY, RKEY_Sat, 0 );
-			}
-			else // otherwise call command handler
-				cmdParser( line );
-
-			// display new prompt
-			if ( echo )
-				TAP_Print("\n=> ");
-
-			// reset line
-			length = 0;
+			// no check for running recordings needed, this has the same effect as
+			// pressing the power button
+			exTAP_Shutdown();
 			break;
-		case '\b':
-			// backspace
-			if ( length > 0)
+		}
+		case 4:
+		{
+			if ( IsRecording(0) || IsRecording(1) )
+				TAP_Print("Not rebooting, recording in progress\n");
+			else
 			{
-				if ( echo)
-					TAP_Print("\b \b");
-				length--;
+				dword cmdBlock[10];
+				cmdBlock[0] = 1;
+				cmdBlock[1] = 4;
+				exTAP_CallFirmware( (void*)0x80000c68, 14, (dword)cmdBlock, 0, 0);
 			}
 			break;
+		}
+		case 5:
+		{
+			//dword addr = exTAP_TAP_Load(line+7);
+			//if ( addr )
+			//	TAP_Print("Loaded %s at %08X\n", line+7, addr);
+			//else
+			//	TAP_Print("Failed to load %s\n", line+7);
+			break;
+		}
+		case 6:
+		{
+			dword size, free, available;
+			TAP_MemInfo( &size, &free, &available );
+			TAP_Print("Memory size:%d free:%d available:%d\n", size, free, available);
+			break;
+		}
+		case 7:
+		{
+			dword state, substate;
+			TAP_GetState( &state, &substate );
+			TAP_Print("State:%d substate:%d\n", state, substate);
+			break;
+		}
+		case 8:
+		{
+			TAP_Print("Virtual remote: TAP_SetSystemVar(%d,%d)\n", cmd.param1, cmd.param2);
+			TAP_SetSystemVar( cmd.param1, cmd.param2 );
+			break;
+		}
 		default:
-			if ( (length == 0 && ch == 0x20) || ch < 0x20 || ch > 0x7e )
-			{
-				// ignore character
-				break;
-			}
-
-			// display character
-			if ( echo )
-				TAP_PutCh( ch );
-
-			// copy character to line buffer
-			line[length] = ch;
-			length++;
+			TAP_Print("Virtual remote: Unknown command: %d (%d,%d)\n", cmd.command, cmd.param1, cmd.param2);
+			break;
 		}
-
-		readIndex = (readIndex + 1) % RX_BUF_SIZE;
+	}
+	else
+	{
+		// Failure reply
+		TAP_PutByte(0);
+		TAP_PutByte(236);
+		TAP_Print("Virtual remote: Ignored command with bad CRC\n");
 	}
 }
+
 
 //-----------------------------------------------------------------------------
 dword TAP_EventHandler( word event, dword param1, dword param2 )
 {
-	if ( event == EVT_IDLE)
+	if ( event == EVT_IDLE )
 	{
-		int *preadIndex = &readIndex;
-		int *pwriteIndex = &writeIndex;
-		char *pRxBuffer = rxBuffer;
-
-		// read from serial port until empty
-		while ( TAP_KbHit() > 0 )
+		// When the Stop Recording? or Check your reservation messages are shown, EVT_UART isn't passed to the TAP.
+		if ( TAP_KbHit() > 0 )
 		{
-			// read character to buffer
-			pRxBuffer[*pwriteIndex] = TAP_GetCh();
-
-			// store character in buffer
-			*pwriteIndex = (*pwriteIndex + 1) % RX_BUF_SIZE;
-
-			// check for overflow
-			if (*pwriteIndex == *preadIndex)
+			if ( TAP_GetCh() == 236 )
 			{
-				// increment the readIndex
-				*preadIndex = (*preadIndex + 1) % RX_BUF_SIZE;
+				ProcessInput();
+				return 0;
 			}
 		}
-
-		ProcessInput();
 	}
-	else if ( event == EVT_UART)
+	if ( event == EVT_UART )
 	{
-		return 0;
+		if ( param1 == 236 )
+		{
+			ProcessInput();
+			return 0;
+		}
+
+		return param1;
 	}
 
 	return param1;
 }
 
-//-----------------------------------------------------------------------------
-// firmware signature of the command parser function
-// bytes with the value of 0xff are not compared
-dword cmdParserSignature[] = {
-	0x27bdffe0,
-	0xafb60018,
-	0xafbe001c,
-	0xafbf0014,
-	0x0080f025,
-	0x3c0480ff,
-	0x2484ffff,
-	0x03c02825,
-	0x0c08ffff,
-	0x00000000,
-	0x0040b025,
-	0x2418ffff,
-	0x12d80009,
-	0x00000000,
-	0x3c0480ff,
-	0x2484ffff,
-	0x02c02825,
-	0x03c03025,
-	0x0c08ffff,
-	0x00000000,
-	0x10000006,
-	0x00000000,
-	0x3c0480ff,
-	0x2484ffff,
-	0x03c02825,
-	0x0c02ffff,
-	0x00000000,
-	0x8fbf0014,
-	0x8fb60018,
-	0x8fbe001c,
-	0x27bd0020,
-	0x03e00008,
-	0x00000000
-};
-
-
-dword systemEventProcSignature[] = 
-{
-	0x27bdff90,
-	0xafbf0058,
-	0xafb3005c,
-	0xafb40060,
-	0xafb50064,
-	0xafb60068,
-	0x0cffffff,
-	0xafbe006c,
-	0x0cffffff,
-	0x00000000
-};
-
-
-// Get the dword representing a call to the internal kbhit function
-dword GetKbHitCall()
-{
-	int i;
-	dword* addr;
-
-	// Search for the first call in TAP_KbHit. This should be the internal kbhit function
-	addr = (dword*)TAP_KbHit;
-	for ( i = 0; i < 40 && *addr != JR_CMD; ++i )
-	{
-		if ( (*addr & JAL_MASK) == JAL_CMD )
-			return *addr;
-		++addr;
-	}
-
-	TAP_Print( "Could not locate internal KbHit function %04X\n", _appl_version );
-	return 0;
-}
-
-
-// Steal serial input from the firmware by removing calls to kbhit within TAP_SystemProc and
-// SystemEventProc
-bool StealSerialInput()
-{
-	dword* addr;
-
-	dword kbHitCall = GetKbHitCall();
-	if ( !kbHitCall )
-		return FALSE;
-
-	// Patch TAP_SystemProc
-	FindAndHackFirmware( (dword*)TAP_SystemProc, 40, kbHitCall, 0x0001025 );
-
-	addr = (dword*)FindFirmwareFunction( 
-		systemEventProcSignature, sizeof(systemEventProcSignature), 0x800ff000, 0x80110000 );
-	if ( addr == 0 )
-	{
-		TAP_Print( "Could not locate SystemEventProc function %04X\n", _appl_version );
-		return FALSE;
-	}
-
-	FindAndHackFirmware( addr, 500, kbHitCall, 0x0001025 );
-
-	return TRUE;
-}
 
 
 //-----------------------------------------------------------------------------
 int TAP_Main()
 {
-	dword cmdParserAddr;
-
-	if ( !StealSerialInput() )
-		return 0;
-
-	cmdParserAddr = FindFirmwareFunction( cmdParserSignature, sizeof(cmdParserSignature), 0x80108000, 0x80110000 );
-	if ( cmdParserAddr == 0 )
-		cmdParserAddr = FindFirmwareFunction( cmdParserSignature, sizeof(cmdParserSignature), 0x80230000, 0x80248000 );
-	if ( cmdParserAddr == 0 )
-	{
-		TAP_Print( "Could not locate command interpretter %04X\n", _appl_version );
-		return 0;
-	}
-
-	cmdParser = (CmdParser)CreateAPIWrapper(cmdParserAddr);
-	if ( cmdParser == NULL )
-	{
-		TAP_Print( "Wrapper initialization failed\n" );
-		return 0;
-	}
+	exTAP_Init();
 
 	// print start message
 	TAP_Print("\nVirtual Remote TAP started\n\n");
-	TAP_Print("=> ");
 
 	return 1;
 }
