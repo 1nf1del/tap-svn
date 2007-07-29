13663a432841d8d8806f178074df4ac07d148b25
justason <justason@04cd18b3-d4fa-0310-ad16-b4e37cca70a5>
2007-07-29 13:22:13 +0000

    New interface

--- trunk/taps/fonts/fonts.c
+++ trunk/taps/fonts/fonts.c
@@ -1,12 +1,12 @@
 #include "localtap.h"
-#include "RasterFontBrowser.h"
+#include "RasterFontBrowser_fast.h"
 #include "tapapifix.h"
 #include <string.h>
 #define ID_FREETYPE_TEST			0x07
 
 TAP_ID( ID_FREETYPE_TEST );
 TAP_PROGRAM_NAME("Font test");
-TAP_AUTHOR_NAME("Justason & simonc");
+TAP_AUTHOR_NAME("Justason");
 TAP_DESCRIPTION("FontTest");
 TAP_ETCINFO(__DATE__);
 
@@ -14,113 +14,23 @@ TAP_ETCINFO(__DATE__);
 int TAP_Main(void);
 word rgn;
 int _appl_version = 0x1200;
+int transparency_orig;
 
-
-void Draw1(FONT *font, unsigned char *msg1, int filelen)
-{
-	unsigned char ptr1[256];
-	int err1, width, text_length;
-	int i, j, lines;
-	word *image=NULL;
-
-
-	SetColors(font, COLOR_Yellow, COLOR_DarkBlue);
-	i=j=0;
-	lines=0;
-	do 
-	{
-		if (msg1[i]==0x0A || i==filelen)
-		{
-			strncpy((char*)ptr1, (const char*)(msg1+j), i-j-1);
-			ptr1[i-j-1]=0;
-
-			width=0;
-			err1 = CalcSize(font, ptr1, &width, &text_length);
-			if (err1==0)
-			{
-				err1 = RenderString(font, ptr1, text_length, width, &image);
-				if (err1==0)
-				{
-					TAP_Osd_DrawPixmap(rgn, 40, 100+42*lines, width, font->m_fontHeight, image, FALSE, OSD_1555);
-					TAP_MemFree(image);
-				}
-			}
-			j=i+1;
-			lines++;
-		}
-		i++;
-	} while(i<=filelen);
-}
-
-void Draw2(unsigned char *msg1, int filelen)
-{
-	unsigned char ptr1[256];
-	int i, j, lines;
-	word *image=NULL;
-
-
-	i=j=0;
-	lines=0;
-	do 
-	{
-		if (msg1[i]==0x0A || i==filelen)
-		{
-			strncpy((char*)ptr1, (const char*)(msg1+j), i-j-1);
-			ptr1[i-j-1]=0;
-			TAP_Osd_PutString(rgn, 40, 100+22*lines, 400, ptr1, COLOR_Yellow, COLOR_DarkBlue, 0, FNT_Size_1419, 0);
-			j=i+1;
-			lines++;
-		}
-		i++;
-	} while(i<=filelen);
-}
-
-void Draw3(FONT *font, unsigned char *msg1, int filelen)
-{
-	unsigned char ptr1[256];
-	int err1, width, text_length;
-	int i, j, lines;
-	word *image=NULL;
-
-
-	SetColors(font, COLOR_Yellow, COLOR_DarkBlue);
-
-	i=j=0;
-	lines=0;
-	do 
-	{
-		if (msg1[i]==0x0A || i==filelen)
-		{
-			strncpy((char*)ptr1, (const char*)(msg1+j), i-j-1);
-			ptr1[i-j-1]=0;
-
-			width=0;
-			err1 = CalcSize(font, ptr1, &width, &text_length);
-			if (err1==0)
-			{
-				RenderStringDirect(font, ptr1, text_length, 40, 100+22*lines, width);
-			}
-			j=i+1;
-			lines++;
-		}
-		i++;
-	} while(i<=filelen);
-}
 
 dword TAP_EventHandler( word event, dword param1, dword param2 )
 {
-	static int count = 0, i, err1, filelen;
-	char msg[256], msg1[256], heading[]="Running 100 times";
-	dword start, stop;
-	TYPE_File *f1;
+	static int count = 0, err1, filelen;
+	char heading[]="Astalavista baby";
+	char message[]="Humpty Dumpty sat on a wall";
 	FONT font;
-
 
 
 	if( event == EVT_KEY )
 	{
 		if ( param1 == RKEY_Exit)
 		{
+			TAP_Osd_SetTransparency(rgn, 0xFF - transparency_orig);
+
 			TAP_Osd_Delete( rgn );
 			TAP_EnterNormal();
 			TAP_Exit();
@@ -129,91 +39,22 @@ dword TAP_EventHandler( word event, dword param1, dword param2 )
 		if ( param1 == RKEY_1)
 		{
 			TAP_ExitNormal();
-			TAP_Osd_FillBox( rgn, 20, 20, 680, 536, COLOR_Gray );
-			Draw_Font_String(rgn, 40, 20, heading, "calibri_iso8859-5_21.rasterized", COLOR_Yellow, COLOR_DarkBlue);
+			TAP_Osd_FillBox(rgn, 50, 20, 620, 300, COLOR_DarkBlue);
+			Draw_Font_String(rgn, 55, 20, heading, "segoeui_iso8859-1_20.rasterized", COLOR_Yellow, COLOR_DarkBlue);
 
-			f1 = TAP_Hdd_Fopen("msg_eng.txt");
-			if (f1==0) return 0;
-
-			filelen = TAP_Hdd_Fread((void*)msg1, 1, 330, f1);
-			msg1[filelen]=0;
-
-			err1 = Load_Font(&font, "calibri_iso8859-1_18.rasterized");
+			err1 = Load_Font(&font, "segoeui_iso8859-1_16.rasterized");
 			if (err1!=0) return 0;
-
-			start = TAP_GetTick();
-			for (i=0; i<100; i++)
-			{
-				Draw1(&font, msg1, filelen);
-			}
-			stop = TAP_GetTick();
-
-			TAP_Hdd_Fclose(f1);
+			TAP_Osd_PutS_Font(rgn, 55, 40, 400, message, COLOR_Yellow, COLOR_Blue, &font, ALIGN_LEFT);
+			TAP_Osd_PutS_Font(rgn, 55, 70, 400, message, COLOR_Yellow, COLOR_Blue, &font, ALIGN_CENTER);
+			TAP_Osd_PutS_Font(rgn, 55, 100, 400, message, COLOR_Yellow, COLOR_Blue, &font, ALIGN_RIGHT);
 			Delete_Font(&font);
 
-			TAP_SPrint(msg, "Duration: %lu ticks", stop-start);
-			Draw_Font_String(rgn, 220, 20, msg, "calibri_iso8859-5_21.rasterized", COLOR_Yellow, COLOR_DarkBlue);
-
-			return 0;
-		}
-
-		if ( param1 == RKEY_2)
-		{
-			TAP_ExitNormal();
-			TAP_Osd_FillBox( rgn, 20,20, 680, 536, COLOR_DarkGray );
-			TAP_Osd_PutString(rgn, 40, 20, 600, heading, COLOR_Yellow, COLOR_DarkBlue, 0, FNT_Size_1419, 0);
-
-			f1 = TAP_Hdd_Fopen("msg_eng.txt");
-			if (f1==0) return 0;
-
-			filelen = TAP_Hdd_Fread((void*)msg1, 1, 330, f1);
-			msg1[filelen]=0;
-
-			start = TAP_GetTick();
-			for (i=0; i<100; i++)
-			{
-				Draw2(msg1, filelen);
-			}
-			stop = TAP_GetTick();
-
-			TAP_Hdd_Fclose(f1);
-
-			TAP_SPrint(msg, "Duration: %lu ticks", stop-start);
-			TAP_Osd_PutString(rgn, 40, 400, 600, msg, COLOR_Yellow, COLOR_DarkBlue, 0, FNT_Size_1419, 0);
-
-			return 0;
-		}
-
-
-		if ( param1 == RKEY_3)
-		{
-			TAP_ExitNormal();
-			TAP_Osd_FillBox( rgn, 20, 20, 680, 536, COLOR_Gray );
-			Draw_Font_String(rgn, 40, 20, heading, "calibri_iso8859-5_21.rasterized", COLOR_Yellow, COLOR_DarkBlue);
-
-			f1 = TAP_Hdd_Fopen("msg_eng.txt");
-			if (f1==0) return 0;
-
-			filelen = TAP_Hdd_Fread((void*)msg1, 1, 330, f1);
-			msg1[filelen]=0;
-
-			err1 = Load_Font(&font, "calibri_iso8859-1_18.rasterized");
+			err1 = Load_Font(&font, "segoeui_iso8859-1_20.rasterized");
 			if (err1!=0) return 0;
-
-			InitDirect();
-			start = TAP_GetTick();
-			for (i=0; i<100; i++)
-			{
-				Draw3(&font, msg1, filelen);
-			}
-			stop = TAP_GetTick();
-
-			TAP_Hdd_Fclose(f1);
+			TAP_Osd_PutS_Font(rgn, 55, 140, 100, message, COLOR_Yellow, COLOR_Blue, &font, ALIGN_LEFT);
+			TAP_Osd_PutS_Font(rgn, 55, 170, 100, message, COLOR_Yellow, COLOR_Blue, &font, ALIGN_CENTER);
+			TAP_Osd_PutS_Font(rgn, 55, 200, 100, message, COLOR_Yellow, COLOR_Blue, &font, ALIGN_RIGHT);
 			Delete_Font(&font);
-
-			TAP_SPrint(msg, "Duration: %lu ticks", stop-start);
-			Draw_Font_String(rgn, 220, 20, msg, "calibri_iso8859-5_21.rasterized", COLOR_Yellow, COLOR_DarkBlue);
-
 			return 0;
 		}
 	}
@@ -223,9 +64,11 @@ dword TAP_EventHandler( word event, dword param1, dword param2 )
 
 int TAP_Main(void)
 {
-
 	InitTAPAPIFix();
-	rgn = TAP_Osd_Create( 0, 0, 720, 576, 0, FALSE );
+	transparency_orig = TAP_GetSystemVar( SYSVAR_OsdAlpha );
+	rgn = TAP_Osd_Create(0, 0, 720, 576, 0, 0);
+
+	TAP_Osd_SetTransparency( rgn, 150 );
 
 	return 1;
 }
